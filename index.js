9d195a985d55a8d55b902b46bc65612ccaaab8d3
SAMUDRALA ARAVIND <67514829+SAMUDRALAARAVIND@users.noreply.github.com>
2021-02-17 11:42:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,91 +1,68 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
+const mysql = require("mysql");
+// var connection = mysql.createConnection({
+//     host:"localhost",
+//     user:"root",
+//     password:"Indian@123",
+//     database:"fyle"
+// });
+// connection.connect();
+var port = process.env.port  || 8080;
+app.set("views","views");
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+app.listen(port, () => {
+    console.log(`Server started on ${port}`);
+});
+app.get("/",(req,resp)=>{
+  resp.render("test");
+})
+app.get("/angular/",function(req,resp){
+  resp.render("angle");
+});
+
+app.post("/angular/",function(req,resp){
+  console.log("requested the server");
+  connection.query(`SELECT * FROM bank_branches WHERE state='telangana' LIMIT 5;`,function(err,result){
+    if(err) throw err;
+     resp.send(result);
+     console.log(result);
+  });
+});
+
+app.post("/",(req,resp)=>{
+    var state = req.body.state;
+    var searchItem = req.body.searchItem;
+    var sql = `SELECT bank_name FROM bank_branches WHERE state='${state}' and bank_name REGEXP '^[${searchItem}]' limit 2;`;
+    connection.query(sql,function(err,result){
+      if(err) throw err;
+      console.log(`result is ${JSON.stringify(result)} and the type of result is ${typeof(result)}`);
+      resp.send(JSON.stringify(result));
+    });
+});
+app.get("/api/branches/autocomplete",function(req,resp){
+    var object = JSON.stringify(req.query);
+    // console.log(req.query);
+    // var x = {queryData:object};
+    // console.log(x);
+    resp.render("search",{x:object});
+});
+
+// app.get("/",function(req,resp){
+//       var sql = `SELECT * FROM bank_branches WHERE bank_name REGEXP '^state' ORDER BY ifsc LIMIT 10;`;
+//             connection.query(sql,function(err,result){
+//               if(err) throw err;
+//               resp.json(result[0]);
+//             });
+// });
 
 
 
-const port = process.env.port || 8080;
-const bodyParser = require("body-parser");
-const mysql = require("mysql")
-
-app.use(express.static('public'))
-// const Pool = require('pg').Pool;
-//
-// const pool = new Pool({
-//   host:"b74k0s8ewoitjlz66apj-postgresql.services.clever-cloud.com",
+// host:"b74k0s8ewoitjlz66apj-postgresql.services.clever-cloud.com",
 //   user:"uadrje84dlumvndov34z",
 //   password:"tqqAlRTodDtVfRI1ljAK",
 //   database:"b74k0s8ewoitjlz66apj",
 //   port:5432
-// });
-
-
-var conn = mysql.createConnection({
-  host:"localhost",
-  database:"fyle",
-  password:"Indian@123",
-  user:"root"
-});
-conn.connect();
-app.set("views", "views");
-app.set("view engine" ,"ejs");
-app.use(bodyParser.urlencoded({extended:true}));
-app.listen(port,()=>{
-  console.log(`listening on port ${port}`)
-})
-
-app.get("/api/branches/autocomplete",function(req,resp){
-  var data = {
-    limit:req.query.limit,
-    q:req.query.q,
-    offset:req.query.offset
-  };
-  resp.render('home',{ejsData:data});
-})
-
-app.post("/",(req,resp)=>{
-  console.log(`state is: ${req.body.state} limit is: ${req.body.limit} searchItem is: ${req.body.searchItem}`);
-  var searchItem = req.body.searchItem;
-  var state = req.body.state;
-  if(searchItem!==''){
-
-      var sql = `SELECT bank_name FROM bank_branches WHERE bank_name REGEXP '${req.body.searchItem}' and state='${state}' ORDER BY ifsc LIMIT 5;`;
-      conn.query(sql,function(err,result){
-        if(err) throw err;
-        console.log(result);
-
-        resp.send(result);
-      });
-    }
-});
-
-
-
-
-app.post("/bank/table",(req,resp)=>{
-  var limit = req.body.limit;
-  var state = req.body.state;
-  var bank = req.body.searchItem;
-
-  console.log(`limit is: ${limit} state=${state} bank = ${bank}`);
-  var query = `SELECT * FROM bank_branches WHERE state='${state}' and bank_name='${bank}' ORDER BY ifsc LIMIT ${limit};`;
-  conn.query(query,function(err,result){
-    if(err) throw err;
-
-    console.log(result);
-    resp.send(result);
-  })
-})
-
-
-
-
-app.get("/test",(req,resp)=>{
-  pool.query(`select * from users;`,function(err,result){
-    if(err) throw err;
-    console.log(result.rows);
-  });
-  resp.send("HEll")
-});
-
-
-//  var pgsql = `INSERT INTO bank_branches(ifsc,bank_id,branch,address,city,district,state,bank_name) VALUES('${result[i].ifsc}', '${result[i].bank_id}', '${result[i].branch}','${result[i].address}','${result[i].city}','${result[i].district}','${result[i].state}','${result[i].bank_name}');`;
+//  C:/Users/Aravind/OneDrive/Desktop/ermin-admin/fyle-project/indian_banks.sql
